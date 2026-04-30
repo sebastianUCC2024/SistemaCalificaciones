9be526e51e09ce4de59bf3e2644deb703f013cf5
Gabonarvaezm <160960360+Gabonarvaezm@users.noreply.github.com>
2026-04-30 08:37:07 -0500

    añadiendo forms y views

--- sistema_calificaciones/calificacionesEstudiantes/views.py
+++ sistema_calificaciones/calificacionesEstudiantes/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.db.models import Avg
+from .models import Calificacion
+from .forms import CalificacionForm
 
-# Create your views here.
+
+def listar_calificaciones(request):
+    calificaciones = Calificacion.objects.all()
+
+    promedio_general = Calificacion.objects.all().aggregate(
+        Avg('promedio')
+    )['promedio__avg']
+
+    return render(request, 'calificaciones/listar.html', {
+        'calificaciones': calificaciones,
+        'promedio_general': promedio_general
+    })
+
+
+def crear_calificacion(request):
+    if request.method == 'POST':
+        form = CalificacionForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('listar_calificaciones')
+    else:
+        form = CalificacionForm()
+
+    return render(request, 'calificaciones/crear.html', {
+        'form': form
+    })
+
+
+def editar_calificacion(request, id):
+    calificacion = get_object_or_404(Calificacion, id=id)
+
+    if request.method == 'POST':
+        form = CalificacionForm(request.POST, instance=calificacion)
+
+        if form.is_valid():
+            form.save()
+            return redirect('listar_calificaciones')
+    else:
+        form = CalificacionForm(instance=calificacion)
+
+    return render(request, 'calificaciones/editar.html', {
+        'form': form
+    })
+
+
+def eliminar_calificacion(request, id):
+    calificacion = get_object_or_404(Calificacion, id=id)
+
+    if request.method == 'POST':
+        calificacion.delete()
+        return redirect('listar_calificaciones')
+
+    return render(request, 'calificaciones/eliminar.html', {
+        'calificacion': calificacion
+    })
+
+
+def promedio_general(request):
+    promedio = Calificacion.objects.all().aggregate(
+        Avg('promedio')
+    )['promedio__avg']
+
+    return render(request, 'calificaciones/promedio_general.html', {
+        'promedio': promedio
+    })
